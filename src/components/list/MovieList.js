984a061f346df6357b2ea9faf0bf94260bd2e0f9
kalapacsrobert <robert.kalapacs@rabit.hu>
2025-06-13 14:50:52 +0200

    init

---
+++ src/components/list/MovieList.js
@@ -0,0 +1,12 @@
+import { tempMovieData } from "../App";
+import Movie from "./Movie";
+
+export default function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
